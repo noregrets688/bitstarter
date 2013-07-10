4d21eadbc4d5365018e58b9746fb2ea3ab571bbf
Ubuntu <ubuntu@ip-172-31-2-181.us-west-2.compute.internal>
2013-07-10 07:12:37 +0000

    express problem

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
-require ('fs');
+var fs = require ('fs');
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 var buffer = fs.readFile('index.html', function(err, data){
 	if(err) throw err;
-       });
-
-app.get('/', function(request, response) {
+var getApplication = app.get('/', function(request, response) {
 	response.send(buffer.toString());
 });
+    
+       });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
